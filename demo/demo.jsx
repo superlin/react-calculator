b0b7cc129433617510dd850055c7794900626ee0
superlin <superlin1990@gmail.com>
2015-10-22 23:53:40 +0800

    first version of react calculator

---
+++ demo/demo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Calculator from '../src/index';
+
+require('./demo.scss');
+
+React.render(
+    <Calculator />,
+    document.getElementById('demo')
+);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Calculator from './lib/Calculator';
+
+require('./style/index.scss');
+require('./style/flex.scss');
+
+export default Calculator;
